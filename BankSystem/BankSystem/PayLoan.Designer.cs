2dfe00d9bf1ac87c2259fe51518245b299214fc2
Mariam Ayman <121129576+mariam511@users.noreply.github.com>
2024-05-18 20:50:49 +0300

    verify loan

--- BankSystem/BankSystem/PayLoan.Designer.cs
+++ BankSystem/BankSystem/PayLoan.Designer.cs
@@ -28,10 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column8 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            Column7 = new DataGridViewTextBoxColumn();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Franklin Gothic Heavy", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkSlateGray;
+            label1.Location = new Point(22, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(300, 47);
+            label1.TabIndex = 1;
+            label1.Text = "Pending Loans";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.FromArgb(0, 64, 64);
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column8, Column3, Column4, Column5, Column6, Column7 });
+            dataGridView1.Location = new Point(3, 76);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(748, 197);
+            dataGridView1.TabIndex = 67;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column1.HeaderText = "Loan Number";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column2.HeaderText = "Customer Name";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            // 
+            // Column8
+            // 
+            Column8.HeaderText = "Employee Name";
+            Column8.MinimumWidth = 6;
+            Column8.Name = "Column8";
+            Column8.Width = 125;
+            // 
+            // Column3
+            // 
+            Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column3.HeaderText = "Amount";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column4.HeaderText = "Interest Rate";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column5.HeaderText = "Type";
+            Column5.MinimumWidth = 6;
+            Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            Column6.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column6.HeaderText = "Payment Shedule";
+            Column6.MinimumWidth = 6;
+            Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            Column7.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column7.HeaderText = "Loan Status";
+            Column7.MinimumWidth = 6;
+            Column7.Name = "Column7";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.Control;
+            label2.Font = new Font("Franklin Gothic Heavy", 14F);
+            label2.ForeColor = Color.DarkSlateGray;
+            label2.Location = new Point(183, 440);
+            label2.Name = "label2";
+            label2.Size = new Size(162, 30);
+            label2.TabIndex = 68;
+            label2.Text = "Loan Number";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(365, 443);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(233, 27);
+            textBox1.TabIndex = 69;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Franklin Gothic Heavy", 14F);
+            radioButton1.ForeColor = Color.FromArgb(0, 64, 64);
+            radioButton1.Location = new Point(225, 498);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(112, 34);
+            radioButton1.TabIndex = 70;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Accept";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Franklin Gothic Heavy", 14F);
+            radioButton2.ForeColor = Color.Red;
+            radioButton2.Location = new Point(397, 498);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(108, 34);
+            radioButton2.TabIndex = 71;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Reject";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Franklin Gothic Heavy", 14F);
+            button1.ForeColor = Color.FromArgb(0, 64, 64);
+            button1.Location = new Point(326, 578);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 46);
+            button1.TabIndex = 72;
+            button1.Text = "Confirm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // PayLoan
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "PayLoan";
+            Size = new Size(754, 673);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        public DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column8;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        private Label label2;
+        private TextBox textBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Button button1;
     }
 }
--- BankSystem/BankSystem/PayLoan.cs
+++ BankSystem/BankSystem/PayLoan.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace BankSystem
 {
@@ -15,6 +22,97 @@ namespace BankSystem
         public PayLoan()
         {
             InitializeComponent();
+        }
+      
+        public void showTable(object sender, EventArgs e) {
+
+            string _path = AppDomain.CurrentDomain.BaseDirectory;
+            string path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(_path).FullName).FullName).FullName).FullName + "\\DB\\LocalDB.mdf";
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + path + ";Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand command = new SqlCommand();
+            command.Connection = con;
+            command.CommandText = "SELECT* FROM LOAN ;";
+            // "SELECT* FROM LOAN WHERE STATUS = 'Pending';";
+            SqlDataReader reader = command.ExecuteReader();
+            for (int j = 0; reader.Read(); j++)
+            {
+                dataGridView1.Rows.Add();
+                for (int i = 0; i < 8; i++)
+                {
+                
+                    dataGridView1.Rows[j].Cells[i].Value = reader.GetValue(i).ToString();
+                }
+            }
+            con.Close();
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e) {
+            string loanNum = textBox1.Text;
+            string status;
+            if (radioButton1.Checked) { status = "Accepted"; }
+            else if (radioButton2.Checked) { status = "Rejected"; }
+            else { status = "test"; }
+            if ( loanNum == "" )
+            {
+                MessageBox.Show("Please fill all fileds!", "Bank System", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string _path = AppDomain.CurrentDomain.BaseDirectory;
+            string path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(_path).FullName).FullName).FullName).FullName + "\\DB\\LocalDB.mdf";
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + path + ";Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand command = new SqlCommand();
+            command.Connection = con;
+            command.CommandText = "select* from LOAN WHERE LOANNO_ = '" + loanNum + "'";
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (!reader.Read())
+            {
+                MessageBox.Show("This Loan isn't exist!", "Bank System", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                con.Close();
+                return;
+            }
+
+            reader.Close();
+            command.CommandText = "select* from LOAN WHERE LOANNO_ = '" + loanNum + "' AND  STATUS ='Pending' ";
+            reader = command.ExecuteReader();
+            if (!reader.Read())
+            {
+                MessageBox.Show("This Loan isn't Pending!", "Bank System", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                con.Close();
+                return;
+            }
+            reader.Close();
+            command.CommandText = "UPDATE LOAN SET EMPLOYEEID =  '"+ Program.employeeID +"', STATUS= '" + status + "'WHERE LOANNO_ = '" + loanNum + "';";
+
+            MessageBox.Show("Loan Request Updated  Successfully!", "BankSystem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            con.Close();
+
+
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
